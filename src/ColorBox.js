18571acc1209da40ea4217308d163a0347546b5f
Sai <saicharan13@gmail.com>
2019-06-19 13:29:42 +0530

    Add copy to clipboard feature

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import "./ColorBox.css";
 
 function ColorBox({ background, name }) {
-  return (
-    <div className="ColorBox" style={{ background }}>
-      <div className="copy-container">
-        <div className="box-content">
-          <span>{name}</span>
-        </div>
-        <button className="copy-button">Copy</button>
-      </div>
+  const [copied, setCopied] = useState(false);
 
-      <div className="see-more">More</div>
-    </div>
+  function changeCopyState() {
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1500);
+  }
+
+  return (
+    <CopyToClipboard text={background} onCopy={changeCopyState}>
+      <div className="ColorBox" style={{ background }}>
+        <div
+          className={`copy-overlay ${copied && "show"}`}
+          style={{ background }}
+        />
+
+        <div className={`copy-msg ${copied && "show"}`}>
+          <h1>Copied!</h1>
+          <p>{background}</p>
+        </div>
+
+        <div className="copy-container">
+          <div className="box-content">
+            <span>{name}</span>
+          </div>
+          <button className="copy-button">Copy</button>
+        </div>
+
+        <div className="see-more">More</div>
+      </div>
+    </CopyToClipboard>
   );
 }
 
