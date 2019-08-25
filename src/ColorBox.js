daced7f1db9c66b921af91375d8f9af6ec2e78e7
Sai <saicharan13@gmail.com>
2019-08-25 17:11:17 +0530

    Set text color to based on background lightness

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import chroma from "chroma-js";
 import "./ColorBox.css";
 
 function ColorBox({ background, name, moreUrl, showLink }) {
   const [copied, setCopied] = useState(false);
+
+  const isDarkColor = chroma(background).luminance() <= 0.08;
+  const isLightColor = chroma(background).luminance() >= 0.7;
 
   function changeCopyState() {
     setCopied(true);
@@ -24,19 +28,23 @@ function ColorBox({ background, name, moreUrl, showLink }) {
 
         <div className={`copy-msg ${copied && "show"}`}>
           <h1>Copied!</h1>
-          <p>{background}</p>
+          <p className={isLightColor ? "dark-text" : ""}>{background}</p>
         </div>
 
         <div className="copy-container">
           <div className="box-content">
-            <span>{name}</span>
+            <span className={isDarkColor ? "light-text" : ""}>{name}</span>
           </div>
-          <button className="copy-button">Copy</button>
+          <button className={`copy-button ${isLightColor ? "dark-text" : ""}`}>
+            Copy
+          </button>
         </div>
 
         {showLink && (
           <Link to={moreUrl} onClick={e => e.stopPropagation()}>
-            <div className="see-more">More</div>
+            <div className={`see-more ${isLightColor ? "dark-text" : ""}`}>
+              More
+            </div>
           </Link>
         )}
       </div>
